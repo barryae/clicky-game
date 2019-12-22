729a10bda2c83cee2c571bc2cabed84a5533813d
Green Vine <barryaervin@gmail.com>
2019-12-22 15:38:52 -0500

    Updates

---
+++ precache-manifest.d3bb54f3c5c155ff064dbda145f991ca.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4449c61d108347548c2912f4a4903dbd",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "7676a9a619ed33056541",
+    "url": "/clicky-game/static/js/2.96b072e9.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/clicky-game/static/js/2.96b072e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bd549e9c6c3e5c50db8",
+    "url": "/clicky-game/static/js/main.3fbb6b6d.chunk.js"
+  },
+  {
+    "revision": "76da5bcc0e2cfb61c97a",
+    "url": "/clicky-game/static/js/runtime-main.39dcd64a.js"
+  },
+  {
+    "revision": "ac3a04117d1d297699aa97ca3a1c512a",
+    "url": "/clicky-game/static/media/bird1.ac3a0411.jpg"
+  },
+  {
+    "revision": "fb96837eb1d93b7281c9b63df013aa16",
+    "url": "/clicky-game/static/media/bird10.fb96837e.jpg"
+  },
+  {
+    "revision": "836a848abe030cbcc41059361e1abef5",
+    "url": "/clicky-game/static/media/bird11.836a848a.jpg"
+  },
+  {
+    "revision": "09c3c5720ba33d8ff5b7489014db305b",
+    "url": "/clicky-game/static/media/bird12.09c3c572.jpg"
+  },
+  {
+    "revision": "3389cb2d71411a5ebe7a9cf216869ecc",
+    "url": "/clicky-game/static/media/bird2.3389cb2d.jpg"
+  },
+  {
+    "revision": "7354752b2b8e69b719fdbaf608e706de",
+    "url": "/clicky-game/static/media/bird3.7354752b.png"
+  },
+  {
+    "revision": "63873b65e457f70f2e32f8d63a6bd503",
+    "url": "/clicky-game/static/media/bird4.63873b65.jpg"
+  },
+  {
+    "revision": "a31f782d5d416b5e7a076630da96ec25",
+    "url": "/clicky-game/static/media/bird5.a31f782d.jpg"
+  },
+  {
+    "revision": "36add9e175545cf625128c1aee8a5a6e",
+    "url": "/clicky-game/static/media/bird6.36add9e1.jpg"
+  },
+  {
+    "revision": "34001aaa0cb834bccfd5f5996d992ad1",
+    "url": "/clicky-game/static/media/bird7.34001aaa.jpg"
+  },
+  {
+    "revision": "22b16a2d8f291efb6b0d3712617c0c70",
+    "url": "/clicky-game/static/media/bird8.22b16a2d.jpg"
+  },
+  {
+    "revision": "c7dfc3384b525aa2927bfff01bade3e2",
+    "url": "/clicky-game/static/media/bird9.c7dfc338.jpg"
+  }
+]);
